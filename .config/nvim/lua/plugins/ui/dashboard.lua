dd35f95a2c3a95a122252cc13dc4dabe0600ba67
connorakey <connorakey@proton.me>
2025-08-09 10:27:32 +1000

    Removed neovim dashboard
    Removed neovim dashboard due to bloat

--- .config/nvim/lua/plugins/ui/dashboard.lua
+++
@@ -1,9 +0,0 @@
-# File for the configuration of alpha-nvim (A dashboard for neovim)
-
-return
-{
-    'goolord/alpha-nvim',
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.dashboard'.config)
-    end
-};
