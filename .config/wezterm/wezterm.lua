abe77e4283a295f973a3e9ac31eee1e7e18ae71d
connorakey <connorakey@proton.me>
2025-08-16 20:03:37 +1000

    Inital commit
    Initial commit with tmux, zsh, and wezterm configuration

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,39 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+-- Font Settings
+config.font_size = 12
+config.font = wezterm.font("JetBrains Mono Nerd Font")
+
+-- Colors
+config.color_scheme = "Catppuccin Frappe"
+
+-- Other Configurations
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
+
+config.keys = {
+	{
+		key = "e",
+		mods = "CTRL",
+		action = wezterm.action.SpawnCommandInNewTab({
+			args = { "nvim" },
+		}),
+	},
+	{
+		key = "q",
+		mods = "CTRL",
+		action = wezterm.action.SpawnCommandInNewTab({
+			args = { "tmux" },
+		}),
+	},
+}
+
+return config
