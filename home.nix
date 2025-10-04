3c1fed14a774eeb26534ac3339e4ed8b90534008
connorakey <connorakey@proton.me>
2025-10-04 16:56:29 +1000

    add mkOutOfStoreSymlink to neovim
    mkOutOfStoreSymlink is a requirement for LazyVim and any other configuration that needs to dynamically alter the config

--- home.nix
+++ home.nix
@@ -1,12 +1,20 @@
 { config, pkgs, ... }:
 
+let
+  dotfiles = "${config.home.homeDirectory}/.nixos/config";
+  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+in 
+
 {
   home.username = "connor";
   home.homeDirectory = "/home/connor";
   programs.git.enable = true;
   home.stateVersion = "25.05";
 
-  home.file.".config/nvim".source = ./config/nvim;
+  xdg.configFile.nvim = {
+      source = create_symlink "/home/connor/.nixos/config/nvim/";
+      recursive = true;
+    };
 
   home.packages = with pkgs; [
     nixpkgs-fmt
