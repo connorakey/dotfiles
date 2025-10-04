b46fd05a38a890b6af0966bab6488ee34eae9dd7
connorakey <connorakey@proton.me>
2025-10-04 23:29:20 +1000

    modify dwm.sh

--- scripts/dwm.sh
+++ scripts/dwm.sh
@@ -1,2 +1,2 @@
-xinput set-prop 14 "libinput Accel Profile Enabled" 0 1 &
-xinput set-prop 14 "libinput Accel Speed" 0 &
+xinput set-prop 20 "libinput Accel Profile Enabled" 0 1 &
+xinput set-prop 20 "libinput Accel Speed" 0 &
